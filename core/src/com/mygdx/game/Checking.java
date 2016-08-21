06e5698f35098e276d76e5e6af66502dc09beb9b
Meekins Works <meekinsworks@outlook.com>
2016-08-20 20:37:30 -0400

    Initial commit

---
+++ core/src/com/mygdx/game/Checking.java
@@ -0,0 +1,18 @@
+package com.mygdx.game;/**
+ * Created by meekinsworks on 8/19/16.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Checking extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ core/src/com/mygdx/game/check.java
@@ -0,0 +1,7 @@
+package com.mygdx.game;
+
+/**
+ * Created by meekinsworks on 8/19/16.
+ */
+public class check {
+}
